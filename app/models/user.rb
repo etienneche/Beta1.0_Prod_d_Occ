6c98fa503424c5c933182b2f1e317ec49332fa02
etienne cherri <etienne@cherri.fr>
2020-04-07 12:00:32 +0200

    simple_form_for with type user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum user_type: { "client" => 0,
+                    "member" => 1 }
 end
